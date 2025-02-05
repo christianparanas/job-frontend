edf68018769d93a024df1e172cc5f41c165bf8e0
Christian Paranas <dev.christianparanas@gmail.com>
2025-02-05 22:47:03 +0800

    resetpass

--- src/app/core/pages/forgotpassword/forgotpassword.component.ts
+++ src/app/core/pages/forgotpassword/forgotpassword.component.ts
@@ -2,47 +2,73 @@ import { Component, OnInit } from '@angular/core';
 import { PasswordModule } from 'primeng/password';
 import { InputTextModule } from 'primeng/inputtext';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
 
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { CommonModule, Location } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { AuthService } from '../../shared/services/auth.service';
+import { HotToastService } from '@ngxpert/hot-toast';
 
 @Component({
   selector: 'app-forgotpassword',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    InputIconModule,
+    IconFieldModule,
+    InputTextModule,
+    ButtonModule,
+    FormsModule,
+  ],
   standalone: true,
   templateUrl: './forgotpassword.component.html',
   styleUrl: './forgotpassword.component.css',
 })
 export class ForgotpasswordComponent {
-  form: FormGroup;
+  email: string = '';
   submitLoading: boolean = false;
   isEmailSent: boolean = false;
 
-  constructor(private location: Location) {}
+  constructor(
+    private location: Location,
+    private authService: AuthService,
+    private toast: HotToastService
+  ) {}
 
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-    });
+  ngOnInit(): void {}
+
+  isValidEmail(email: string) {
+    // Regular expression for validating an email
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
   }
 
   onSubmit() {
-    // if(!this.form.valid) return this.toast.info("Please enter a valid email address")
+    if (this.email == '' && !this.isValidEmail(this.email)) {
+      this.toast.info('Please enter a valid email address');
+    }
 
+    // Start loading spinner
     this.submitLoading = true;
 
-    // this.authService.forgotpassword(this.form.value).subscribe((response: any) => {
-    //   this.isEmailSent = true
-    //   this.submitLoading = false
-    // },
-    // (err: any) => {
-    //   this.submitLoading = false
-    //   this.toast.error(err.error.message)
-    // })
-  }
-
-  goBack() {
-    this.location.back();
+    // Call the forgot password service method
+    this.authService.forgotPassword(this.email).subscribe(
+      (response: any) => {
+        // Handle successful response
+        this.isEmailSent = true; // You can use this to show a success message or redirect to another page
+        this.submitLoading = false;
+      },
+      (err: any) => {
+        // Handle error response
+        this.submitLoading = false;
+        this.toast.error(
+          err.error.message || 'Something went wrong. Please try again.'
+        );
+      }
+    );
   }
 }
--- src/app/core/pages/resetpassword/resetpassword.component.ts
+++ src/app/core/pages/resetpassword/resetpassword.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { HotToastService } from '@ngxpert/hot-toast';
+import { AuthService } from '../../shared/services/auth.service';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-resetpassword',
-  imports: [ReactiveFormsModule, FormsModule, CommonModule],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+    RouterModule,
+    InputIconModule,
+    IconFieldModule,
+    InputTextModule,
+    ButtonModule,
+    PasswordModule,
+  ],
   standalone: true,
   templateUrl: './resetpassword.component.html',
   styleUrl: './resetpassword.component.css',
@@ -21,43 +38,52 @@ export class ResetpasswordComponent implements OnInit {
 
   submitLoading: boolean = false;
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private toast: HotToastService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-    // this.route.queryParams.subscribe((value) => {
-    //   (this.token = value.token), (this.userId = value.id);
-    // });
+    this.route.queryParams.subscribe((value) => {
+      this.token = value['token'];
+    });
 
-    if (this.token == null && this.userId == null) {
-      this.router.navigate(['/']);
+    if (this.token == null) {
+      this.router.navigate(['/login']);
     }
   }
 
   onSubmit() {
-    if (this.password != this.confirmPassword)
-      // return this.toast.error("Password didn't matched.");
+    if (this.password != this.confirmPassword) {
+      this.toast.error("Password didn't matched.");
+      return;
+    }
 
-      this.submitLoading = true;
+    this.submitLoading = true;
 
     const data = {
       token: this.token,
       password: this.password,
-      userId: this.userId,
     };
 
-    // this.authService.resetpassword(data).subscribe(
-    //   (response: any) => {
-    //     this.submitLoading = false;
-    //     this.toast.success(response.message);
+    // Call the forgot password service method
+    this.authService.resetPassword(data).subscribe(
+      (response: any) => {
+        // Handle successful response
+        this.submitLoading = false;
+        this.toast.success(response.message);
 
-    //     this.router.navigate([`/login`], {
-    //       queryParams: { type: 'student' },
-    //     });
-    //   },
-    //   (err: any) => {
-    //     this.submitLoading = false;
-    //     this.toast.error(err.error.message);
-    //   }
-    // );
+        this.router.navigate([`/login`]);
+      },
+      (err: any) => {
+        // Handle error response
+        this.submitLoading = false;
+        this.toast.error(
+          err.error.message || 'Something went wrong. Please try again.'
+        );
+      }
+    );
   }
 }
